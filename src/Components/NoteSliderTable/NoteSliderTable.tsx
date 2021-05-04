1ad37ce2edb904d890ce0745d3025288b56931d5
Joel Boersma <joelboersma@gmail.com>
2021-05-04 13:43:06 -0700

    table made with map and css grid

--- src/Components/NoteSliderTable/NoteSliderTable.tsx
+++ src/Components/NoteSliderTable/NoteSliderTable.tsx
@@ -3,28 +3,14 @@ import NoteSlider from '../NoteSlider/NoteSlider';
 import './NoteSliderTable.scss'
 
 const NoteSliderTable: FC = () => {
+  // TODO: make vals SliderValue type
+  let sliderVals: Number[] = Array(16).fill(0);
+  
   return (
     <div className="NoteSliderTable">
-      <div className="NoteSliderTableRow" id="Row1">
-        <NoteSlider id={0}/>
-        <NoteSlider id={1}/>
-        <NoteSlider id={2}/>
-        <NoteSlider id={3}/>
-        <NoteSlider id={4}/>
-        <NoteSlider id={5}/>
-        <NoteSlider id={6}/>
-        <NoteSlider id={7}/>
-      </div>
-      <div className="NoteSliderTableRow" id="Row2">
-        <NoteSlider id={8}/>
-        <NoteSlider id={9}/>
-        <NoteSlider id={10}/>
-        <NoteSlider id={11}/>
-        <NoteSlider id={12}/>
-        <NoteSlider id={13}/>
-        <NoteSlider id={14}/>
-        <NoteSlider id={15}/>
-      </div>
+      {
+        sliderVals.map((val, index) => <NoteSlider id={index}/>)
+      }
     </div>
   );
 }
