d7f48ee4f3db8af5142b20f101d74bc4e3cf6337
Joel Boersma <joelboersma@gmail.com>
2021-11-25 10:58:57 -0800

    make app explicitly-typed FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './App.scss';
 import NoteSliderTable from './Components/NoteSliderTable/NoteSliderTable'
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
       <header>
