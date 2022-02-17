3639d10712e12dc6ebea66a416a5a956b09845ab
Joel Boersma <joelboersma@gmail.com>
2022-02-16 20:22:40 -0800

    SharePopup: copy button & styling

--- src/Components/SharePopup.tsx
+++ src/Components/SharePopup.tsx
@@ -12,13 +12,19 @@ const SharePopup = (props: SharePopupProps) => {
       const NOTE_HEX = props.notes.map(note => note.value.toString(16)).join('');
       return `${window.location.origin}/${NOTE_HEX}`;
    }
+   const SHARE_URL = parseUrl()
+
+   const copyUrl = () => {
+      navigator.clipboard.writeText(SHARE_URL)
+   }
 
    return (
       <div id="PopupBackground" className={props.enabled ? '' : 'disabled'}>
          <div className="card">
             <h2>Share</h2>
             <p>Use the link to share your tune!</p>
-            <input readOnly={true} value={parseUrl()}></input>
+            <input readOnly={true} value={SHARE_URL}></input>
+            <button id="CopyButton" onClick={copyUrl}>Copy</button>
          </div>
       </div>
    )
